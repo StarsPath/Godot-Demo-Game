a4299b20811744ef238de035914037b26723f383
LawrenceZChen <lawrencechen8922@gmail.com>
2023-12-17 21:18:54 -0500

    Removed gravity

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 30.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,10 +20,8 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if vector:
-		velocity = vector * SPEED
-	elif not is_on_floor():
-		velocity.y += gravity * delta
-	else:
-		velocity = lerp(velocity, Vector2.ZERO, 0.1)
+		velocity += vector * SPEED
+	#else:
+		#velocity = lerp(velocity, Vector2.ZERO, 0.5)
 
 	move_and_slide()
